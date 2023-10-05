a13cbdf3ea51feebb47b26c50cfd5bc8983051dd
Trym Auren <trym@eduroam-193-157-248-149.wlan.uio.no>
2023-10-05 12:15:05 +0200

    Structuring of code

--- not_trash_code/testing/fitness_evaluation.py
+++ not_trash_code/testing/fitness_evaluation.py
@@ -4,20 +4,36 @@ def evaluate_individual(individual) -> tuple:
 	"""
 	Evaluates the fitness of an individuals genes. Used by DEAP.
 		Arg: individual
-		Ret: --
 	"""
 	
-	limbs = list(np.array_split(individual,12))
-	for i in limbs:
-		actions = sin_func(*i)
-	fitness = evaluate(unity)
+	movement = compute_movement(individual)
+	fitness = send_actions_to_unity(movement)
+
 	return (fitness,) # must return tuple!
 
-def sin_func(A, f, phase):
-	# Create an array of linearly spaced values for x
-    x_values = np.linspace(0, 2 * np.pi, 12)
+def compute_movement(individual) -> np.array(np.array):
+	"""
+	Fetches a sinusoid for each limb and returns a 2d list
+	containing sinusoid for all limbs.
+		Arg: an individual (the individual must inherit from python list).
+	"""
+	limbs = list(np.array_split(individual,12))
+	movement = [0]*len(limbs)
+	for i in range(len(limbs)):
+		sinusoid = compute_sin(*(limbs[i]))
+		movement[i] = sinusoid
+	return movement
 
-    # Compute the sine values
-    sin_values = (A*np.sin(f * x_values + phase)+A)/(2*A)
-    continuous_action = sin_values.reshape(1, 12).astype(np.float32)
-    return continuous_action
+def compute_sin(A=1, f=2, phase=0) -> np.array:
+	"""
+	Computes a sinusoid for the given parameters.
+		Arg: A: amplitude, f: frequency, phase: -.-
+	"""
+	t = np.array([0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1], dtype=float)
+	sinusoid = (np.sin(t * f + phase)+A)/(2*A)
+	return sinusoid
+
+
+# mat =  [1,1,0.25, 1,1,0.25, 1,1,0.25, 1,1,0.25,
+# 		1,1,0.5, 1,1,0.5, 1,1,0.5, 1,1,0.5,
+# 		2,1,0.5, 2,1,0.5, 2,1,0.5, 2,1,0.5]
