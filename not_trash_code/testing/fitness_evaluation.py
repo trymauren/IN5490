8ee7ff6568eb15114feb79d37bc10da8db709f68
Trym Auren <trym@eduroam-193-157-182-115.wlan.uio.no>
2023-10-11 17:54:53 +0200

    Add a few functions

--- not_trash_code/testing/fitness_evaluation.py
+++ not_trash_code/testing/fitness_evaluation.py
@@ -1,14 +1,23 @@
 import numpy as np
 
+def evaluate_population(population, test_mode = False) -> None:
+	if not test_mode:
+		for individual in population:
+			individual.fitness.values = evaluate_individual(individual)
+	else:
+		fitnesses = [0]*len(population)
+		for i in range(len(population)):
+			fitnesses[i] = evaluate_individual(population[i])
+		return fitnesses
+
 def evaluate_individual(individual) -> tuple:
 	"""
 	Evaluates the fitness of an individuals genes. Used by DEAP.
 		Arg: individual
 	"""
-	
 	movement = compute_movement(individual)
-	fitness = send_actions_to_unity(movement)
-
+	# fitness = send_actions_to_unity(movement)
+	fitness = movement
 	return (fitness,) # must return tuple!
 
 def compute_movement(individual) -> np.array(np.array):
@@ -34,6 +43,7 @@ def compute_sin(A=1, f=2, phase=0) -> np.array:
 	return sinusoid
 
 
-# mat =  [1,1,0.25, 1,1,0.25, 1,1,0.25, 1,1,0.25,
-# 		1,1,0.5, 1,1,0.5, 1,1,0.5, 1,1,0.5,
-# 		2,1,0.5, 2,1,0.5, 2,1,0.5, 2,1,0.5]
+mat =  [1,1,0.25, 1,1,0.25, 1,1,0.25, 1,1,0.25,
+		1,1,0.5, 1,1,0.5, 1,1,0.5, 1,1,0.5,
+		2,1,0.5, 2,1,0.5, 2,1,0.5, 2,1,0.5]
+print(evaluate_population([mat,mat], test_mode=True))
