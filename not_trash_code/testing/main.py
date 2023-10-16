6074bd8f127fee5a634da77aa91ebfabc895b0fa
trymauren <trym.auren@gmail.com>
2023-10-16 15:27:07 +0200

    Add stop_env()

--- not_trash_code/testing/main.py
+++ not_trash_code/testing/main.py
@@ -5,11 +5,11 @@ from utils import unity_stuff
 
 import os
 # Set path for data
-executable_path = os.path.abspath('') #Working directory path (Where program is run from)
+executable_path = os.path.abspath('..') #Working directory path (Where program is run from)
 executable_path += '/executables/'
 
 def main():
-	unity_interface = unity_stuff.UnityInterface(executable_file=functions.get_executable(executable_path), no_graphics=True)
+	unity_interface = unity_stuff.UnityInterface(executable_file=functions.get_executable(executable_path), no_graphics=False, worker_id=1)
 	best = basic_deap.train(train_config, unity_interface)
 	print(best)
     
