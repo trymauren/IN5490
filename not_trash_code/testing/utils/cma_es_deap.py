00334edac34631596fe384e49b5451bb49fafe7e
trymauren <trym.auren@gmail.com>
2023-10-26 22:37:57 +0200

    Add more statistics logging

--- not_trash_code/testing/utils/cma_es_deap.py
+++ not_trash_code/testing/utils/cma_es_deap.py
@@ -12,22 +12,26 @@
 #
 #    You should have received a copy of the GNU Lesser General Public
 #    License along with DEAP. If not, see <http://www.gnu.org/licenses/>.
-
-"""Implementation of the BI-Population CMA-ES algorithm. As presented in
-*Hansen, 2009, Benchmarking a BI-Population CMA-ES on the BBOB-2009 Function
-Testbed* with the exception of the modifications to the original CMA-ES
-parameters mentioned at the end of section 2's first paragraph.
-"""
-
+# 
+#    Implementation of the BI-Population CMA-ES algorithm. As presented in
+#    *Hansen, 2009, Benchmarking a BI-Population CMA-ES on the BBOB-2009 Function
+#    Testbed* with the exception of the modifications to the original CMA-ES
+#    parameters mentioned at the end of section 2's first paragraph.
 
 
 
-#https://deap.readthedocs.io/en/master/api/algo.html#module-deap.cma
+# See documentation:
+#   https://deap.readthedocs.io/en/master/api/algo.html#module-deap.cma
+#
+#   Statistics:
+#   https://deap.readthedocs.io/en/master/tutorials/basic/part3.html
+
+
 
 from utils import fitness_evaluation
 from collections import deque
 
-import numpy
+import numpy as np
 
 from deap import algorithms
 from deap import base
@@ -36,43 +40,66 @@ from deap import cma
 from deap import creator
 from deap import tools
 
-# Problem size
+# Problem size (genome length)
 N = 36
 
-creator.create("FitnessMin", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMin)
+creator.create('FitnessMin', base.Fitness, weights=(1.0,))
+creator.create('Individual', list, fitness=creator.FitnessMin)
 
 def train(unity_interface, verbose=True):
-    numpy.random.seed(128)
+    np.random.seed(128)
     NRESTARTS = 10  # Initialization + 9 I-POP restarts
     SIGMA0 = 2.0    # 1/5th of the domain [-5 5]
 
     toolbox = base.Toolbox()
-    toolbox.register("evaluate", fitness_evaluation.evaluate_population)
 
+    # Register custom fitness evaluation function
+    toolbox.register('evaluate', fitness_evaluation.evaluate_population)
+
+    # Create the hall of fame (the container storing the best individuals)
     halloffame = tools.HallOfFame(1)
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", numpy.mean)
-    stats.register("std", numpy.std)
-    stats.register("min", numpy.min)
-    stats.register("max", numpy.max)
 
+    # TODO: move these to functions.py or something else
+    def amplitude(ind):
+        return np.mean(np.array([ind[i] for i in range(0,len(ind),3)]))
+
+    def frequency(ind):
+        return np.mean(np.array([ind[i] for i in range(1,len(ind),3)]))
+
+    def phase_shift(ind):
+        return np.mean(np.array([ind[i] for i in range(2,len(ind),3)]))
+
+    # Create the statistics functions
+    stats_fitness       = tools.Statistics(lambda ind: ind.fitness.values)
+    stats_amplitude     = tools.Statistics(amplitude)
+    stats_frequency     = tools.Statistics(frequency)
+    stats_phase_shift   = tools.Statistics(phase_shift)
+    # Register statistics functions
+    stats = tools.MultiStatistics(  fitness=stats_fitness, amplitude=stats_amplitude, 
+                                    frequency=stats_frequency, phase_shift=stats_phase_shift)
+    stats.register('avg', np.mean)
+    stats.register('std', np.std)
+    stats.register('min', np.min)
+    stats.register('max', np.max)
+
+    # Create a list to store all the logbooks. One logbook is created for each run.
     logbooks = list()
 
     nsmallpopruns = 0
     smallbudget = list()
     largebudget = list()
-    lambda0 = 4 + int(3 * numpy.log(N))
+    # lambda0 = 4 + int(3 * np.log(N)) # population size - dynamic, dependent on individual size
+    lambda0 = 30 # population size - static
     regime = 1
+    
     i = 0
-
     while i < (NRESTARTS + nsmallpopruns):
         # The first regime is enforced on the first and last restart
         # The second regime is run if its allocated budget is smaller than the allocated
         # large population regime budget
         if i > 0 and i < (NRESTARTS + nsmallpopruns) - 1 and sum(smallbudget) < sum(largebudget):
-            lambda_ = int(lambda0 * (0.5 * (2**(i - nsmallpopruns) * lambda0) / lambda0)**(numpy.random.rand()**2))
-            sigma = 2 * 10**(-2 * numpy.random.rand())
+            lambda_ = int(lambda0 * (0.5 * (2**(i - nsmallpopruns) * lambda0) / lambda0)**(np.random.rand()**2))
+            sigma = 2 * 10**(-2 * np.random.rand())
             nsmallpopruns += 1
             regime = 2
             smallbudget += [0]
@@ -86,18 +113,18 @@ def train(unity_interface, verbose=True):
 
         # Set the termination criterion constants
         if regime == 1:
-            MAXITER = 100 + 50 * (N + 3)**2 / numpy.sqrt(lambda_)
+            MAXITER = 100 + 50 * (N + 3)**2 / np.sqrt(lambda_)
         elif regime == 2:
             MAXITER = 0.5 * largebudget[-1] / lambda_
-            
+          
         TOLHISTFUN = 10**-12
-        TOLHISTFUN_ITER = 10 + int(numpy.ceil(30. * N / lambda_))
+        TOLHISTFUN_ITER = 10 + int(np.ceil(30. * N / lambda_))
         EQUALFUNVALS = 1. / 3.
-        EQUALFUNVALS_K = int(numpy.ceil(0.1 + lambda_ / 4.))
+        EQUALFUNVALS_K = int(np.ceil(0.1 + lambda_ / 4.))
         TOLX = 10**-12
         TOLUPSIGMA = 10**20
         CONDITIONCOV = 10**14
-        STAGNATION_ITER = int(numpy.ceil(0.2 * t + 120 + 30. * N / lambda_))
+        STAGNATION_ITER = int(np.ceil(0.2 * t + 120 + 30. * N / lambda_))
         NOEFFECTAXIS_INDEX = t % N
 
         equalfunvalues = list()
@@ -106,35 +133,50 @@ def train(unity_interface, verbose=True):
         mins = deque(maxlen=TOLHISTFUN_ITER)
 
         # We start with a centroid in [-4, 4]**D
-        strategy = cma.Strategy(centroid=numpy.random.uniform(-4, 4, N), sigma=sigma, lambda_=lambda_)
-        toolbox.register("generate", strategy.generate, creator.Individual)
-        toolbox.register("update", strategy.update)
+        strategy = cma.Strategy(centroid=np.random.uniform(-4, 4, N), sigma=sigma, lambda_=lambda_)
+        toolbox.register('generate', strategy.generate, creator.Individual)
+        toolbox.register('update', strategy.update)
 
+        # Create a new toolbox for each restart
         logbooks.append(tools.Logbook())
-        logbooks[-1].header = "gen", "evals", "restart", "regime", "std", "min", "avg", "max"
+        logbooks[-1].header = 'gen', 'evals', 'restart', 'regime', 'fitness', 'amplitude', 'frequency', 'phase_shift'
+        logbooks[-1].chapters['fitness'].header = 'std', 'min', 'avg', 'max'
+        logbooks[-1].chapters['amplitude'].header = 'std', 'min', 'avg', 'max'
+        logbooks[-1].chapters['frequency'].header = 'std', 'min', 'avg', 'max'
+        logbooks[-1].chapters['phase_shift'].header = 'std', 'min', 'avg', 'max'
 
-        conditions = {"MaxIter" : False, "TolHistFun" : False, "EqualFunVals" : False,
-                      "TolX" : False, "TolUpSigma" : False, "Stagnation" : False,
-                      "ConditionCov" : False, "NoEffectAxis" : False, "NoEffectCoor" : False}
+        # Create stopping criteria dict
+        conditions = {'MaxIter' : False, 'TolHistFun' : False, 'EqualFunVals' : False,
+                      'TolX' : False, 'TolUpSigma' : False, 'Stagnation' : False,
+                      'ConditionCov' : False, 'NoEffectAxis' : False, 'NoEffectCoor' : False}
 
 
         # Run the current regime until one of the following is true:
         ## Note that the algorithm won't stop by itself on the optimum (0.0 on rastrigin).
         while not any(conditions.values()):
+            
             # Generate a new population
             population = toolbox.generate()
 
-            # Evaluate the individuals
-            # fitnesses = toolbox.map(toolbox.evaluate, population, unity_interface)
-            fitnesses = toolbox.evaluate(population, unity_interface)
+            # Evaluate the individuals:
+            # The following commented out line is how DEAP suggests to calculate fitness
+            # fitnesses = toolbox.map(toolbox.evaluate, population, unity_interface) # fuck this
 
+            # Here, it is done with the following to make evaluation parallell
+            # (evaluate many individuals) in Unity.
+            fitnesses = toolbox.evaluate(population, unity_interface) # very nice
+
+            # Assign the computed fitness to individuals
             for ind, fit in zip(population, fitnesses):
                 ind.fitness.values = fit
 
-
+            # update hall of fame with current populations best k individuals
             halloffame.update(population)
+
+            # This creates stats of current population using the stats.register('') above
             record = stats.compile(population)
             logbooks[-1].record(gen=t, evals=lambda_, restart=i, regime=regime, **record)
+
             if verbose:
                 print(logbooks[-1].stream)
 
@@ -156,57 +198,54 @@ def train(unity_interface, verbose=True):
             elif regime == 2:
                 smallbudget[-1] += lambda_
 
+            # Here comes lots of stopping-criteria stuff, not so important
             t += 1
-            STAGNATION_ITER = int(numpy.ceil(0.2 * t + 120 + 30. * N / lambda_))
+            STAGNATION_ITER = int(np.ceil(0.2 * t + 120 + 30. * N / lambda_))
             NOEFFECTAXIS_INDEX = t % N
 
             if t >= MAXITER:
                 # The maximum number of iteration per CMA-ES ran
-                conditions["MaxIter"] = True
+                conditions['MaxIter'] = True
 
-            mins.append(record["min"])
+            mins.append(logbooks[-1].chapters["fitness"].select("min"))
             if (len(mins) == mins.maxlen) and max(mins) - min(mins) < TOLHISTFUN:
                 # The range of the best values is smaller than the threshold
-                conditions["TolHistFun"] = True
+                conditions['TolHistFun'] = True
 
             if t > N and sum(equalfunvalues[-N:]) / float(N) > EQUALFUNVALS:
                 # In 1/3rd of the last N iterations the best and k'th best solutions are equal
-                conditions["EqualFunVals"] = True
+                conditions['EqualFunVals'] = True
 
-            if all(strategy.pc < TOLX) and all(numpy.sqrt(numpy.diag(strategy.C)) < TOLX):
+            if all(strategy.pc < TOLX) and all(np.sqrt(np.diag(strategy.C)) < TOLX):
                 # All components of pc and sqrt(diag(C)) are smaller than the threshold
-                conditions["TolX"] = True
+                conditions['TolX'] = True
 
-            # Need to transfor strategy.diagD[-1]**2 from pyp/numpy.float64 to python
+            # Need to transfor strategy.diagD[-1]**2 from pyp/np.float64 to python
             # float to avoid OverflowError
             if strategy.sigma / sigma > float(strategy.diagD[-1]**2) * TOLUPSIGMA:
                 # The sigma ratio is bigger than a threshold
-                conditions["TolUpSigma"] = True
+                conditions['TolUpSigma'] = True
 
             if len(bestvalues) > STAGNATION_ITER and len(medianvalues) > STAGNATION_ITER and \
-               numpy.median(bestvalues[-20:]) >= numpy.median(bestvalues[-STAGNATION_ITER:-STAGNATION_ITER + 20]) and \
-               numpy.median(medianvalues[-20:]) >= numpy.median(medianvalues[-STAGNATION_ITER:-STAGNATION_ITER + 20]):
+               np.median(bestvalues[-20:]) >= np.median(bestvalues[-STAGNATION_ITER:-STAGNATION_ITER + 20]) and \
+               np.median(medianvalues[-20:]) >= np.median(medianvalues[-STAGNATION_ITER:-STAGNATION_ITER + 20]):
                 # Stagnation occurred
-                conditions["Stagnation"] = True
+                conditions['Stagnation'] = True
 
             if strategy.cond > 10**14:
                 # The condition number is bigger than a threshold
-                conditions["ConditionCov"] = True
+                conditions['ConditionCov'] = True
 
             if all(strategy.centroid == strategy.centroid + 0.1 * strategy.sigma * strategy.diagD[-NOEFFECTAXIS_INDEX] * strategy.B[-NOEFFECTAXIS_INDEX]):
                 # The coordinate axis std is too low
-                conditions["NoEffectAxis"] = True
+                conditions['NoEffectAxis'] = True
 
-            if any(strategy.centroid == strategy.centroid + 0.2 * strategy.sigma * numpy.diag(strategy.C)):
+            if any(strategy.centroid == strategy.centroid + 0.2 * strategy.sigma * np.diag(strategy.C)):
                 # The main axis std has no effect
-                conditions["NoEffectCoor"] = True
+                conditions['NoEffectCoor'] = True
 
         stop_causes = [k for k, v in conditions.items() if v]
-        print("Stopped because of condition%s %s" % ((":" if len(stop_causes) == 1 else "s:"), ",".join(stop_causes)))
+        print('Stopped because of condition%s %s' % ((':' if len(stop_causes) == 1 else 's:'), ','.join(stop_causes)))
         i += 1
 
     return halloffame
-
-if __name__ == "__main__":
-    best = main()
-    print(best)
