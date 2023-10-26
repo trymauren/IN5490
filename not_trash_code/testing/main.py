48314cbe18db4455f0e316b5602496d0290825aa
trymauren <trym.auren@gmail.com>
2023-10-26 10:46:12 +0200

    Small change

--- not_trash_code/testing/main.py
+++ not_trash_code/testing/main.py
@@ -1,4 +1,5 @@
 from utils import basic_deap
+from utils import cma_es_deap
 from config import train_config
 from utils import functions
 from utils import unity_stuff
@@ -6,28 +7,29 @@ from utils import fitness_evaluation
 
 import os
 # Set path for data
-executable_path = os.path.abspath('') #Working directory path (Where program is run from)
-executable_path += '/executables/'
+root = os.path.abspath('../')
+executable_path = root + '/executables/'
 
 def main():
 	params = {'executable_file':functions.get_executable(executable_path),'no_graphics':True, 'worker_id':1}
 	unity_interface = unity_stuff.UnityInterface(**params)
-	best = basic_deap.train(train_config, unity_interface)
-	print(best)
+	# best = basic_deap.train(train_config, unity_interface)
+	halloffame = cma_es_deap.train(unity_interface)
+	print(halloffame)
     
-	unity_interface.stop_env() #funker dette ??
-	visualization = False
-	best_res = None
+	# unity_interface.stop_env() #funker dette ??
+	# visualization = False
+	# best_res = None
       
-	if visualization:
-		vis_unity_interface = unity_stuff.UnityInterface(executable_file=functions.get_executable(executable_path), no_graphics=False, worker_id=1)
-		fitness_evaluation.evaluate_population(best_res, vis_unity_interface)
-		vis_unity_interface.stop_env()
-	else:
-		unity_interface = unity_stuff.UnityInterface(executable_file=functions.get_executable(executable_path), no_graphics=False, worker_id=1)
-		best = basic_deap.train(train_config, unity_interface)
-		print(best)
-		unity_interface.stop_env() #funker dette ??
+	# if visualization:
+	# 	vis_unity_interface = unity_stuff.UnityInterface(executable_file=functions.get_executable(executable_path), no_graphics=False, worker_id=1)
+	# 	fitness_evaluation.evaluate_population(best_res, vis_unity_interface)
+	# 	vis_unity_interface.stop_env()
+	# else:
+	# 	unity_interface = unity_stuff.UnityInterface(executable_file=functions.get_executable(executable_path), no_graphics=False, worker_id=1)
+	# 	best = basic_deap.train(train_config, unity_interface)
+	# 	print(best)
+	# 	unity_interface.stop_env() #funker dette ??
 		
 
 if __name__ == "__main__":
