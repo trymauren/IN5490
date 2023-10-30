b7fd5b5290a4864acd73c6670dfa5a26b83bebab
trymauren <trym.auren@gmail.com>
2023-10-30 20:32:51 +0100

    Add more config dicts

--- not_trash_code/testing/config.py
+++ not_trash_code/testing/config.py
@@ -1,7 +1,12 @@
 train_config = {
-'IND_SIZE' : 36,
-'NGEN' : 100,
-'POP_SIZE' : 60,
-'CXPB' : 0.1,
-'MUTPB' : 0.1,
+'IND_SIZE' 			: 36,
+'NGEN' 				: 100,
+'POP_SIZE' 			: 60,
+'CXPB' 				: 0.1,
+'MUTPB' 			: 0.1,
+}
+
+interface_config = {
+'no_graphics'		:False,
+'worker_id'			:1
 }
