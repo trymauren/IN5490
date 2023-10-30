9ed18a4f1ccb7d6f316ad59e0fed2471a3ba2a1f
oaoverbo <oaoverbo@uio.no>
2023-10-30 19:48:17 +0100

    fix

--- not_trash_code/testing/main.py
+++ not_trash_code/testing/main.py
@@ -6,6 +6,7 @@ from config import train_config
 from utils import functions
 from utils import unity_stuff
 from utils import fitness_evaluation
+import numpy as np
 
 import os
 # Set path for data
@@ -15,10 +16,18 @@ executable_path = root + '/executables/'
 def main():
 	params = {'executable_file':functions.get_executable(executable_path),'no_graphics':False, 'worker_id':1}
 	unity_interface = unity_stuff.UnityInterface(**params)
+	# unity_interface = unity_stuff.UnityInterface(executable_file=None, no_graphics=False, worker_id=0)
 	# best = basic_deap.train(train_config, unity_interface)
 	logbooks, halloffame = cma_es_deap.train(unity_interface)
 	pickle.dump(logbooks, f'logbook_{time.time()}')
+
+	# fitness_evaluation.simulate_best(individ, 20, unity_interface)
 	unity_interface.stop_env() #funker dette ?? kanskje, ja!!!!
+
+      
+
+      
+	
 	
 
 if __name__ == "__main__":
