86d814d518ca6921d8946bbf82c36d871aeb8873
trymauren <trym.auren@gmail.com>
2023-11-01 12:49:24 +0100

    Add more configs...

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 ea_config = {
 'pop_size'			: 30,
+'num_generations'	: 600,
 'std_dev'			: 3,
 'genome_len'		: 36,
-'num_mov_repeat'	: 30
+'num_mov_repeat'	: 50,
+'fitness_one_axis'	: True,
+'lower_start_limit'	: -10,
+'upper_start_limit'	: 10,
 }
 
 interface_config = {
-'no_graphics'		:True,
+'no_graphics'		:False,
 'worker_id'			:2
 }
