d9de6eff9d86b38d8a1fccc5a23e66b125816aa5
trymauren <trym.auren@gmail.com>
2023-11-02 14:27:20 +0100

    Cleaned code

--- utils/functions.py
+++ utils/functions.py
@@ -17,24 +17,18 @@ import matplotlib.pyplot as plt
 from config import ea_config
 from tkinter import filedialog as fd
 
-def fetch_timestamp():
-    """Gets time of when function is called
-
-    Returns:
-        datetime: year_month_day_hour_minute_second
-    """
-    return datetime.today().strftime('%Y_%m_%d_%H_%M_%S')
-
 def make_plots_from_logbook(path):
     """Makes plot from data stored in logbook
 
     Args:
         path (tulpe): path to logbook and halloffame in a tuple
     """
-    print(path)
-    path_w_out_extension = os.path.splitext(path[0])[0]
-    file_name = path_w_out_extension[path_w_out_extension.find('_'):][1:] # Gets the date from 
+    # print(path)
+    path_w_out_extension = os.path.splitext(path)[0] # important to make shelve work!
+    file_name = path_w_out_extension[path_w_out_extension.find('_2023'):][1:] # Gets the date from 
     logbooks = []
+    print(path_w_out_extension)
+    print(file_name)
     with shelve.open(path_w_out_extension, 'c') as fp: 
         for i, d in enumerate(fp):
             logbook = fp[str(i)]
@@ -48,34 +42,210 @@ def make_plots_from_logbook(path):
         phase_shift = logbook.chapters['phase_shift'].select('avg')
 
         # Plotting and styling for Average Fitness
-        ax1.plot(fitness, marker='o', linestyle='-', color='b')
+        ax1.plot(fitness)
         ax1.set_title('Average Fitness', fontsize=14)
         ax1.set_xlabel('Generation', fontsize=12)
         ax1.set_ylabel('Fitness', fontsize=12)
         ax1.grid(True)
 
         # Plotting and styling for Average Frequency
-        ax2.plot(max_fitness, marker='s', linestyle='--', color='g')
+        ax2.plot(max_fitness)
         ax2.set_title('Max Fitness', fontsize=14)
         ax2.set_xlabel('Generation', fontsize=12)
-        ax2.set_ylabel('Frequency', fontsize=12)
+        ax2.set_ylabel('Fitness', fontsize=12)
         ax2.grid(True)
 
         # Plotting and styling for Average Amplitude
-        ax3.plot(amplitude, marker='x', linestyle='-.', color='r')
+        ax3.plot(amplitude)
         ax3.set_title('Average Amplitude', fontsize=14)
         ax3.set_xlabel('Generation', fontsize=12)
         ax3.set_ylabel('Amplitude', fontsize=12)
         ax3.grid(True)
 
         # Plotting and styling for Average Phase Shift
-        ax4.plot(phase_shift, marker='^', linestyle=':', color='m')
+        ax4.plot(phase_shift)
         ax4.set_title('Average Phase Shift', fontsize=14)
         ax4.set_xlabel('Generation', fontsize=12)
         ax4.set_ylabel('Phase Shift', fontsize=12)
         ax4.grid(True)
     plt.savefig(f'4xplot_{file_name}.pdf',dpi=300)
     print(' -- Made plots')
+
+
+def fetch_halloffame_data(path):
+    """Reads HOF data from file and visualizes the agent with highest fitness
+
+    Args:
+        path(selve.DbfilenameShelf,selve.DbfilenameShelf): Tuple of shelve.DbfilenameShelf file paths
+
+    Returns:
+        data[][]: Nested list of all parameters for amplitude, frequency and phase shift for the best fitness
+    """
+
+    path_w_out_extension = os.path.splitext(path[1])[0]
+    data = []
+    with shelve.open(path_w_out_extension, 'c') as fp: 
+        print(type(fp))
+        for i, d in enumerate(fp):
+            data.append(fp[str(i)])
+    return data
+
+def sim_best(runs_path, executable_path):
+    """
+    Simulate the best solution from the hall of fame.
+    
+    Args:
+        runs_path (str): Path to the directory where simulation runs are stored.
+        executable_path (str): Path to the executable file.
+        
+    Returns:
+        None
+    """
+    timestamp = input('Timestamp to simulate: \'enter\' to use latest, \'n\' to navigate\n')
+    if timestamp == 'n':
+        path_to_halloffame = fd.askopenfilename()
+    else:
+        path_to_halloffame = get_newest_halloffame(runs_path)
+
+    halloffame = get_halloffame_data(path_to_halloffame)
+
+    exe_path = get_executable(executable_path, 1)
+    unity_interface = unity_stuff.UnityInterface(executable_file=exe_path, **interface_config)
+
+    fitness_evaluation.simulate_best(halloffame, 500, unity_interface)
+    
+def plot(runs_path):
+    """
+    Plot the results based on a given timestamp or the latest results.
+    
+    Args:
+        runs_path (str): Path to the directory where simulation runs are stored.
+        
+    Returns:
+        None
+    """
+    timestamp = input('Timestamp to plot: \'enter\' to use latest, \'n\' to navigate\n')
+    if timestamp == 'n':
+        path_to_logbook = fd.askopenfilename()
+    else:
+        path_to_logbook = get_newest_logbook(runs_path)
+    make_plots_from_logbook(path_to_logbook)
+
+def train(runs_path, executable_path):
+    """
+    Train the simulation using CMA-ES (Covariance Matrix Adaptation Evolution Strategy).
+    
+    Args:
+        runs_path (str): Path to the directory where simulation runs are stored.
+        executable_path (str): Path to the executable file.
+        
+    Returns:
+        None
+    """
+    # Create the simulation environment
+    exe_path = get_executable(executable_path, ea_config['pop_size'])
+    unity_interface = unity_stuff.UnityInterface(executable_file=exe_path, **interface_config)
+
+    # Run EA
+    logbooks, halloffame = cma_es_deap.train(unity_interface, runs_path)
+
+    # Stop simulation environmentsim
+    unity_interface.stop_env()
+
+    # Write results to file
+    dump_data(logbooks, halloffame, runs_path)
+    
+ 
+def plot_latest(runs_path):
+    """
+    Plot the latest results from the simulation runs.
+    
+    Args:
+        runs_path (str): Path to the directory where simulation runs are stored.
+        
+    Returns:
+        None
+    """
+    path_to_logbook = get_newest_logbook(runs_path)
+    make_plots_from_logbook(path_to_logbook)
+
+
+### --- Read from file --- ###
+def get_newest_logbook(path_to_runs):
+    """
+    Fetches the latest created logbook file paths
+        Arg: Path to the directory containing the data
+        Ret: Paths to the latest created files containing logbook
+    """
+    files = os.listdir(path_to_runs)
+    paths_logbook = [os.path.join(path_to_runs, basename) for basename in files if 'logbook_' in basename]
+    if len(paths_logbook) == 0:
+        print(f' -- Directory {path_to_runs} does not contain any logbook history')
+        return None
+    latest_logbook = max(paths_logbook, key=os.path.getctime)
+    print(f' -- Fetched {latest_logbook}')
+    return latest_logbook
+
+def get_newest_halloffame(path_to_runs):
+    """
+    Fetches the latest created halloffame path
+        Arg: Path to the directory containing the data
+        Ret: Paths to the latest created files containing halloffame
+    """
+    files = os.listdir(path_to_runs)
+    paths_halloffame = [os.path.join(path_to_runs, basename) for basename in files if 'halloffame_' in basename]
+    if len(paths_halloffame) == 0:
+        print(f' -- Directory {path_to_runs} does not contain any halloffame history')
+        return None
+    latest_halloffame = max(paths_halloffame, key=os.path.getctime)
+    print(f' -- Fetched {latest_halloffame}')
+    return latest_halloffame
+
+### --- Write to file --- ###
+def dump_data(logbooks, halloffame, runs_path):
+    """
+    Redericts dumping to the two helpers
+        Arg: List containing Logbook() objects, halloffame containing individual, path to runs dir
+        Ret: None
+    """
+    timestamp = get_timestamp()
+    logbook_path = os.path.join(runs_path, 'logbook_' + timestamp)
+    halloffame_path = os.path.join(runs_path,'halloffame_' + timestamp)
+    dump_logbook(logbooks, logbook_path)
+    dump_halloffame(halloffame, halloffame_path)
+
+def dump_logbook(data, path):
+    """
+    Dumps the data using pickle
+        Arg: List containing Logbook() objects
+        Ret: None
+    """
+
+    with shelve.open(path, 'c') as fp: 
+        for i, d in enumerate(data):
+            fp[str(i)] = d
+    print(' -- Dumped logbooks')
+
+def dump_halloffame(data, path):
+    """
+    Dumps the data using pickle
+        Arg: List containing Logbook() objects
+        Ret: None
+    """
+    with shelve.open(path, 'c') as fp: 
+        for i, d in enumerate(data):
+            fp[str(i)] = d
+    print(' -- Dumped halloffame')
+
+
+### --- Helpers --- ###
+def get_timestamp():
+    """Gets time of when function is called
+
+    Returns:
+        datetime: year_month_day_hour_minute_second
+    """
+    return datetime.today().strftime('%Y_%m_%d_%H_%M_%S')
 
 def get_executable(executable_path, pop_size=ea_config['pop_size']):
     """
@@ -108,186 +278,3 @@ def get_executable(executable_path, pop_size=ea_config['pop_size']):
         
     return ret
 
-def dump_data(logbooks, halloffame, runs_path):
-    """
-    Redericts dumping to the two helpers
-        Arg: List containing Logbook() objects, halloffame containing individual, path to runs dir
-        Ret: None
-    """
-    timestamp = fetch_timestamp()
-    logbook_path = os.path.join(runs_path, 'logbook_' + timestamp)
-    halloffame_path = os.path.join(runs_path,'halloffame_' + timestamp)
-    dump_logbook(logbooks, logbook_path)
-    dump_halloffame(halloffame, halloffame_path)
-
-def dump_logbook(data, path):
-    """
-    Dumps the data using pickle
-        Arg: List containing Logbook() objects
-        Ret: None
-    """
-
-    with shelve.open(path, 'c') as fp: 
-        for i, d in enumerate(data):
-            fp[str(i)] = d
-    print(' -- Dumped logbooks')
-
-def dump_halloffame(data, path):
-    """
-    Dumps the data using pickle
-        Arg: List containing Logbook() objects
-        Ret: None
-    """
-    with shelve.open(path, 'c') as fp: 
-        for i, d in enumerate(data):
-            fp[str(i)] = d
-    print(' -- Dumped halloffame')
-
-def get_newest_file_paths(path_to_dir):
-    """
-    Fetches the latest created logbook and halloffame file paths
-        Arg: Path to the directory containing the data
-        Ret: Paths to the latest created files containing logbook and halloffame
-    """
-    files = os.listdir(path_to_dir)
-    paths_logbook = [os.path.join(path_to_dir, basename) for basename in files if 'logbook_' in basename]
-    paths_halloffame = [os.path.join(path_to_dir, basename) for basename in files if 'halloffame' in basename]
-    if len(paths_halloffame) == 0 or len(paths_logbook) == 0:
-        print(f' -- Directory {path_to_dir} is empty')
-        return None,None
-    latest_logbook = max(paths_logbook, key=os.path.getctime)
-    latest_halloffame = max(paths_halloffame, key=os.path.getctime)
-    print(f' -- Fetched {latest_logbook}')
-    print(f' -- Fetched {latest_halloffame}')
-    return latest_logbook, latest_halloffame
-
-def get_specific_file_paths(path, timestamp):
-    """
-    Fetches specific logbook and halloffame file paths
-        Arg: Path to the directory containing the data
-        Ret: Paths to the files containing logbook and halloffame
-    """
-    files = os.listdir(path)
-    files = [os.path.join(path, basename) for basename in files]
-
-    path_to_logbook = os.path.join(path, 'logbook_') + timestamp
-    path_to_halloffame = os.path.join(path, 'halloffame_') + timestamp
-
-    if path_to_logbook in files:
-        print(f' -- Fetched {path_to_logbook}')
-
-    else:
-        print(f' -- File {path_to_logbook} does not exist')
-        path_to_logbook = None
-
-    if path_to_halloffame in files:
-        print(f' -- Fetched {path_to_halloffame}')
-    
-    else:
-        print(f' -- File {path_to_halloffame} does not exist')
-        path_to_halloffame = None
-
-    return path_to_logbook, path_to_halloffame
-
-def hof_data(path):
-    """Reads HOF data from file and visualizes the agent with highest fitness
-
-    Args:
-        path(selve.DbfilenameShelf,selve.DbfilenameShelf): Tuple of shelve.DbfilenameShelf file paths
-
-    Returns:
-        data[][]: Nested list of all parameters for amplitude, frequency and phase shift for the best fitness
-    """
-
-    path_w_out_extension = os.path.splitext(path[1])[0]
-    data = []
-    with shelve.open(path_w_out_extension, 'c') as fp: 
-        print(type(fp))
-        for i, d in enumerate(fp):
-            data.append(fp[str(i)])
-    return data
-
-def sim_best(runs_path, executable_path):
-    """
-    Simulate the best solution from the hall of fame.
-    
-    Args:
-        runs_path (str): Path to the directory where simulation runs are stored.
-        executable_path (str): Path to the executable file.
-        
-    Returns:
-        None
-    """
-    timestamp = input('Timestamp to simulate: (enter to use latest), n to navigate\n')
-    if timestamp == 'n':
-        path_to_file = fd.askopenfilename()
-    else:
-        path_to_file = get_newest_file_paths(runs_path)
-
-    halloffame = hof_data(path_to_file)
-
-    exe_path = get_executable(executable_path, 1)
-    unity_interface = unity_stuff.UnityInterface(executable_file=exe_path, **interface_config)
-
-    fitness_evaluation.simulate_best(halloffame, 500, unity_interface)
-    
-
-def plot(runs_path):
-    """
-    Plot the results based on a given timestamp or the latest results.
-    
-    Args:
-        runs_path (str): Path to the directory where simulation runs are stored.
-        
-    Returns:
-        None
-    """
-    timestamp = input('Timestamp to plot: \'enter\' to use latest, \'n\' to navigate\n')
-    if timestamp == 'n':
-        print('Pick logbook file')
-        path_to_files = fd.askopenfilename()
-        # print('Pick halloffame file')
-        # path_to_files[1] = fd.askopenfilename()
-        # functions.get_specific_file_paths(runs_path, timestamp)
-    else:
-        path_to_files = get_newest_file_paths(runs_path)
-    make_plots_from_logbook(path_to_files)
-    # functions.make_plots_from_halloffame(path_to_files[1])
-
-def train(runs_path, executable_path):
-    """
-    Train the simulation using CMA-ES (Covariance Matrix Adaptation Evolution Strategy).
-    
-    Args:
-        runs_path (str): Path to the directory where simulation runs are stored.
-        executable_path (str): Path to the executable file.
-        
-    Returns:
-        None
-    """
-    # Create the simulation environment
-    exe_path = get_executable(executable_path, ea_config['pop_size'])
-    unity_interface = unity_stuff.UnityInterface(executable_file=exe_path, **interface_config)
-
-    # Run EA
-    logbooks, halloffame = cma_es_deap.train(unity_interface, runs_path)
-
-    # Stop simulation environmentsim
-    unity_interface.stop_env()
-
-    # Write results to file
-    dump_data(logbooks, halloffame, runs_path)
-    
-    
-def plot_latest(runs_path):
-    """
-    Plot the latest results from the simulation runs.
-    
-    Args:
-        runs_path (str): Path to the directory where simulation runs are stored.
-        
-    Returns:
-        None
-    """
-    path_to_files = get_newest_file_paths(runs_path)
-    make_plots_from_logbook(path_to_files[0])
