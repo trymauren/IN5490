9b6b0f16a1229f8af6fc931aa00a4c8e4a9a3542
trymauren <trym.auren@gmail.com>
2023-11-02 18:47:25 +0100

    Big changes!

--- utils/basic_deap.py
+++ utils/basic_deap.py
@@ -1,59 +1,107 @@
+from deap import algorithms
 from deap import base
-from deap import tools
+from deap import benchmarks
 from deap import creator
+from deap import tools
 import random
 from utils import fitness_evaluation
 from utils import functions
+from config import ea_config
+import numpy as np
+# http://deap.gel.ulaval.ca/doc/default/overview.html
 
+def train(unity_interface, runs_path, verbose=True):
+    print('-- Running basic_deap')
+    lambda_ = ea_config['pop_size']
+    N = ea_config['genome_len']
+    lower_start_limit = ea_config['lower_start_limit']
+    upper_start_limit = ea_config['upper_start_limit']
 
-def train(train_config, unity_interface):
+    creator.create('FitnessMax', base.Fitness, weights=(1.0,))
+    creator.create('Individual', list, fitness=creator.FitnessMax)
 
+    np.random.seed(ea_config['seed'])
+    # print(np.random.uniform(ea_config['lower_start_limit'], ea_config['upper_start_limit'], N))
+    # exit()
     toolbox = base.Toolbox()
+
+    toolbox.register("attribute", np.random.uniform, lower_start_limit, upper_start_limit)
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attribute, n=N)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual, n=lambda_)
+
+    toolbox.register('evaluate', fitness_evaluation.evaluate_population)
+    toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.1)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+
+    # Create the hall of fame (the container storing the best individuals)
     halloffame = tools.HallOfFame(1)
+
+    # Create the statistics functions
+    def amplitude(ind):
+        return np.mean(np.array([ind[i] for i in range(0,len(ind),3)]))
+    def frequency(ind):
+        return np.abs(np.mean(np.array([ind[i] for i in range(1,len(ind),3)])))
+    def phase_shift(ind):
+        return np.mean(np.array([ind[i] for i in range(2,len(ind),3)]))
+
+    stats_fitness       = tools.Statistics(lambda ind: ind.fitness.values)
+    stats_amplitude     = tools.Statistics(amplitude)
+    stats_frequency     = tools.Statistics(frequency)
+    stats_phase_shift   = tools.Statistics(phase_shift)
     
-    creator.create('FitnessMin', base.Fitness, weights=(1.0,))
-    creator.create('Individual', list, fitness=creator.FitnessMin)
+    # Register and create statistics functions
+    stats = tools.MultiStatistics(  fitness=stats_fitness, amplitude=stats_amplitude, 
+                                    frequency=stats_frequency, phase_shift=stats_phase_shift)
+    
+    stats.register('avg', lambda x: np.mean(x))
+    stats.register('std', lambda x: np.std(x))
+    stats.register('min', lambda x: np.min(x))
+    stats.register('max', lambda x: np.max(x))
 
-    toolbox.register('attr_float', random.random)
-    toolbox.register('individual', tools.initRepeat, creator.Individual,
-                     toolbox.attr_float, n=train_config['IND_SIZE'])
+    # Generate the initial population
 
-    toolbox.register('mate', tools.cxTwoPoint)
-    toolbox.register('mutate', tools.mutGaussian, mu=0, sigma=1, indpb=0.2)
-    toolbox.register('select', tools.selTournament, tournsize=3)
-    toolbox.register('evaluate_population', fitness_evaluation.evaluate_population)
-    toolbox.register('population', tools.initRepeat, list, toolbox.individual)
+    logbook = tools.Logbook()
+    logbook.header = 'gen', 'fitness', 'amplitude', 'frequency', 'phase_shift'
+    logbook.chapters['fitness'].header = 'std', 'min', 'avg', 'max'
+    logbook.chapters['amplitude'].header = 'std', 'min', 'avg', 'max'
+    logbook.chapters['frequency'].header = 'std', 'min', 'avg', 'max'
+    logbook.chapters['phase_shift'].header = 'std', 'min', 'avg', 'max'
+    
+    pop = toolbox.population(n=ea_config['pop_size'])
+    CXPB, MUTPB, NGEN = 0.5, 0.2, ea_config['num_generations']
 
-    population = toolbox.population(n=train_config['POP_SIZE'])
-
-    for g in range(train_config['NGEN']):
-        offspring = toolbox.select(population, len(population))
+    for g in range(NGEN):
+        
+        # Select the next generation individuals
+        offspring = toolbox.select(pop, len(pop))
+        # print(np.shape(offspring))
+        # Clone the selected individuals
         offspring = list(map(toolbox.clone, offspring))
 
+        # Apply crossover and mutation on the offspring
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
-            if random.random() < train_config['CXPB']:
+            if random.random() < CXPB:
                 toolbox.mate(child1, child2)
                 del child1.fitness.values
                 del child2.fitness.values
 
         for mutant in offspring:
-            if random.random() < train_config['MUTPB']:
+            if random.random() < MUTPB:
                 toolbox.mutate(mutant)
                 del mutant.fitness.values
 
+        # Evaluate the entire population
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        # fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
-        # fitnesses = toolbox.map(toolbox.evaluate, [env, invalid_ind])
-        # for ind, fit in zip(invalid_ind, fitnesses):
-        #     ind.fitness.values = fit
 
-        # for ind, fit in zip(invalid_ind, fitnesses):
-        #     ind.fitness.values = fit
-
-        toolbox.evaluate_population(invalid_ind, unity_interface)
-        population[:] = offspring
-
-        halloffame.update(population)
-
-
-    return population
+        fitnesses = toolbox.evaluate(invalid_ind, unity_interface) # very nice
+        # Assign the computed fitness to individuals
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+        # The population is entirely replaced by the offspring
+        pop[:] = offspring
+        record = stats.compile(pop)
+        logbook.record(gen=g, **record)
+        if verbose:
+            print(logbook.stream)
+    return pop
