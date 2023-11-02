e07fd08e7ca806bb3f10f5f9766bbd96adf5747f
trymauren <trym.auren@gmail.com>
2023-11-02 20:25:25 +0100

    Finished up the work

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 ea_config = {
+# 'ea_type'						: 'cma_es_deap_without_restarts',
 'ea_type'						: 'basic_deap',
 'pop_size'						: 60,
-'num_generations'				: 600, # only appliccable for basic_ea
+'num_generations'				: 2, # only appliccable for basic_ea
 'std_dev'						: 3,
-'genome_len'					: 36,
+'genome_len'					: 25,
 'num_mov_repeat'				: 50,
-'fitness_one_axis'				: True,
+'fitness_one_axis'				: False,
 'lower_start_limit'				: -10,
 'upper_start_limit'				: 10,
 'num_restarts'					: 10,
 'seed'							: 128,
-'equal_frequency_all_limbs'		: False,
+'equal_frequency_all_limbs'		: True,
 'basic_ea'						: False,
 }
 
