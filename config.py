ee4a763f1ae8f25173385c4717397a0730db67dd
jonasrom <jonasrom@uio.no>
2023-11-12 21:05:00 +0100

    t

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 ea_config = {
 # 'ea_type'						: 'cma_es_bipop',
- 'ea_type'						: 'cma_es',
-# 'ea_type'						: 'basic',
+# 'ea_type'						: 'cma_es',
+ 'ea_type'						: 'basic',
 'pop_size'						: 60,
 'num_generations'				: 100,
 'std_dev'						: 3,
 'genome_len'					: 36,
 'num_mov_repeat'				: 50, 		# 50 is fine
 'fitness_one_axis'				: False,
-'lower_start_limit'				: -10,
-'upper_start_limit'				: 10,
+'lower_start_limit'				: -1,
+'upper_start_limit'				: 1,
 'num_restarts'					: 10,		# not appliccable for bipop
 'seed'							: 128,		# should really not be necessary to adjust
 'equal_frequency_all_limbs'		: False,
