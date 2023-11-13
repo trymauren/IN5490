c622866055870cc13f964ddaa41c8a25054ab224
trymauren <trym.auren@gmail.com>
2023-11-13 12:21:18 +0100

    Change in sim_best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils_threaded_high import functions
-from config import sim_config
+from config import ea_config, sim_config
 import os
 import sys
 import numpy
@@ -9,7 +9,7 @@ def main():
 	runs_path = os.path.join(root,'runs/')
 
 	if 'train' in sys.argv:
-		functions.train(10)
+		functions.train(ea_config['num_restarts'])
   
 		if 'plot' in sys.argv:
 			functions.plot_latest(runs_path)
@@ -21,7 +21,7 @@ def main():
 		functions.make_combined_plots_from_logbook(runs_path)
 
 	if 'sim_best' in sys.argv:
-		functions.sim_best(runs_path, executable_path)
+		functions.sim_best(runs_path, executable_path, 10)
 
 if __name__ == "__main__":
     main()
